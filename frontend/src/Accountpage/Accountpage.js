f3a311b4c6ffdcdc29e937e4c47711ee7dc1faaf
David Lee <davidlee1813@gmail.com>
2024-02-23 11:01:45 -0800

    initial account page setup

---
+++ frontend/src/Accountpage/Accountpage.js
@@ -0,0 +1,14 @@
+import NavBarComponent from "../Components/NavBarComponent/NavBarComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProfileInfo from "../Components/ProfileInfo/ProfileInfo";
+
+function Accountpage() {
+  return (
+    <span>
+	  <NavBarComponent/>
+      <ProfileInfo/>
+    </span>
+  );
+}
+
+export default Accountpage;
