b2905a390eb36ca3c5f9e70b14e34060a2766add
Enoch Xu <nxtmonkey@gmail.com>
2024-03-07 14:46:49 -0800

    fix merge conflicts

--- frontend/src/Components/RestroomReviews/RestroomReviews.js
+++ frontend/src/Components/RestroomReviews/RestroomReviews.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import "./RestroomReviews.css";
 import RestroomRating from "../RestroomRating/RestroomRating";
 import Rating from "react-rating";
@@ -14,6 +13,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar as fasStar } from "@fortawesome/free-solid-svg-icons";
 import { faStar as farStar } from "@fortawesome/free-regular-svg-icons";
 import Form from "react-bootstrap/Form";
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+
+function get_gender_string(genders) {
+  let str = "";
+  if (genders.indexOf("male") > -1) {
+    str += "Male, ";
+  }
+  if (genders.indexOf("female") > -1) {
+    str += "Female, ";
+  }
+  if (genders.indexOf("all gender") > -1) {
+    str += "Gender Neutral, ";
+  }
+  if (str.length - 2 > 11) {
+    return "All Options";
+  }
+  return str.substring(0, str.length - 2);
+}
 
 function RestroomReviews() {
   const filledStar = <FontAwesomeIcon icon={fasStar} />;
@@ -22,6 +41,45 @@ function RestroomReviews() {
   const writeReviewClicked = () => {
     setAppearReview(!appearReview);
   };
+
+  const [bathroom, setBathroom] = useState({
+    building: "",
+    floor: "",
+    address: "",
+    rating: null,
+    number_ratings: 0,
+    genders: ["Male"],
+  });
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [reviews, setReviews] = useState([]);
+  function getInformation() {
+    let bathroom_id = searchParams.get("_id");
+    axios
+      .get("http://localhost:4000/get-bathroom")
+      .then((response) => {
+        console.log(response);
+        for (let i = 0; i < response.data.length; i++) {
+          if (response.data[i].bathroom_id == bathroom_id) {
+            setBathroom(response.data[i]);
+          }
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    axios
+      .get("http://localhost:4000/get-review?bathroomId=" + bathroom_id)
+      .then((response) => {
+        console.log("Setting Responses");
+        setReviews(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  useEffect(() => {
+    getInformation();
+  }, []);
 
   return (
     <div className="restroom-reviews">
@@ -33,17 +91,17 @@ function RestroomReviews() {
       </div>
       <div className="description">
         <div className="address" style={{ justifyContent: "center" }}>
-            <h2 className="subtitle bathroom-name">{bathroom.building + " " + bathroom.floor}</h2>
+          <h2 className="subtitle bathroom-name">
+            {bathroom.building + " " + bathroom.floor}
+          </h2>
           <p>
             <img src={PinEmoji} alt="pin" />{" "}
             <Badge bg="primary" style={{ justifyContent: "center" }}>
-		{bathroom.address}
+              {bathroom.address}
             </Badge>
           </p>
         </div>
-        <RestroomRating
-            data={bathroom}
-        />
+        <RestroomRating data={bathroom} />
       </div>
       <div className="description-border"></div>
       <div className="actions">
@@ -60,14 +118,14 @@ function RestroomReviews() {
             {" "}
             <WcIcon />{" "}
           </div>
-            <Badge bg="dark">{get_gender_string(bathroom.genders)}</Badge>
+          <Badge bg="dark">{get_gender_string(bathroom.genders)}</Badge>
         </p>
         <p>
           <div style={{ color: "grey", transform: "scale(1.3)" }}>
             {" "}
             <DirectionsWalkIcon />{" "}
           </div>
-            <Badge bg="dark"> {searchParams.get("_dist")} Feet </Badge>
+          <Badge bg="dark"> {searchParams.get("_dist")} Feet </Badge>
         </p>
       </div>
       {appearReview && (
@@ -87,18 +145,14 @@ function RestroomReviews() {
               <Form.Control className="review-box" as="textarea" rows={3} />
             </Form.Group>
           </Form>
-          <Button variant="secondary">
-            SUBMIT
-          </Button>
+          <Button variant="secondary">SUBMIT</Button>
         </div>
       )}
       <div className="reviews-wrap">
-          <div className="reviews">
-	      {
-		  reviews.map((review) => {
-		     return <RestroomReview data={review}/>
-		  })
-	      }
+        <div className="reviews">
+          {reviews.map((review) => {
+            return <RestroomReview data={review} />;
+          })}
         </div>
       </div>
     </div>
