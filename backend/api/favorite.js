030ecbce38ab84f8a46f4de6dac742f721b0cc78
Allen Zhang <allenusca@gmail.com>
2024-03-09 13:26:04 -0800

    Favorites stuff

--- backend/api/favorite.js
+++ backend/api/favorite.js
@@ -52,6 +52,12 @@ const changeFavoriteBathroom = async (req, res) => {
   userId = parseInt(userId, 10);
   bathroomId = parseInt(bathroomId, 10);
 
+const addFavoriteBathroom = async (req, res) => {
+  let { bathroomId, userId } = req.body;
+
+    userId = parseInt(userId, 10);
+    bathroomId = parseInt(bathroomId, 10);
+    
   try {
     const db = await connectToDatabase();
     const userCollection = db.collection("users");
