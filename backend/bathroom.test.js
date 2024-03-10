6ee950bc7f313c71a999e079e546eefcbe7e775f
Enoch Xu <nxtmonkey@gmail.com>
2024-03-10 16:37:03 -0700

    more tests!

--- backend/bathroom.test.js
+++ backend/bathroom.test.js
@@ -2,20 +2,67 @@ require('text-encoding-utf-8');
 const request = require("supertest")
 const { app, startServer } = require("./server")
 beforeAll(async () => {
-  // Start your server here
   await startServer();
 });
 
 describe("Get bathroom test", () => {
-  jest.setTimeout(10000);
   test("GET /get-bathroom", (done) => {
     request(app)
       .get("/get-bathroom")
+      .expect("Content-Type", /json/)
       .expect(200).end((err, res) => {
         if (err) return done(err);
         return done();
       });
-      // Even more logic goes here
   });
-  // More things come here
+});
+
+describe("Get review success test", () => {
+  test("GET /get-review", (done) => {
+    const queryParams = {
+      bathroomId: '11',
+      userId: '37492047'
+    };
+    request(app)
+      .get("/get-review")
+      .query(queryParams)
+      .expect(200).end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
+
+describe("Get review non existent", () => {
+  test("GET /get-review", (done) => {
+    const queryParams = {
+      bathroomId: '8',
+      userId: '37492047'
+    };
+    request(app)
+      .get("/get-review")
+      .query(queryParams)
+      .expect(404).end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
+
+describe("add review test", () => {
+  test("POST /add-review", (done) => {
+
+    const requestBody = {
+      bathroomId: '9',
+      userId: '37492047',
+      rating: '3',
+    };
+    request(app)
+      .post("/add-review")
+      .send(requestBody)
+      .expect(201).end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
 });
